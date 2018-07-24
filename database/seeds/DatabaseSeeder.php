b9b5863614e33d92009391f20049a4043baaaf6e
Rati <rjp76@njit.edu>
2018-07-24 09:40:26 -0400

    Car Table Seeder/Factory Created and running

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,8 +12,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(UsersTableSeeder::class);
-        //$this->call(CarTableSeeder::class);
+        //$this->call(UsersTableSeeder::class);
+        $this->call(CarTableSeeder::class);
 
     }
 }//connect userstableseeder to the database somehow
