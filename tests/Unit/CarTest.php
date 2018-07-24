5299f165f532901e441db570a7ba7d6314f0ccf9
Rati <rjp76@njit.edu>
2018-07-24 10:15:16 -0400

    delete car test created (Test file also renamed)

--- tests/Unit/CarTest.php
+++ tests/Unit/CarTest.php
@@ -7,14 +7,14 @@ use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use App\Car;
 
-class CarInsertTest extends TestCase
+class CarTest extends TestCase
 {
     /**
      * A basic test example.
      *
      * @return void
      */
-    public function testExample()
+    public function testCarInsert()
     {
         $car = new Car();
         $car->Make='Subaru(TEST)';
@@ -22,5 +22,13 @@ class CarInsertTest extends TestCase
         $car->Year=0000;
 
         $this->assertTrue($car->save());
+    }
+    public function testCarDelete(){
+        $car = new Car();
+        $car->Make='Subaru(TEST)';
+        $car->Model ='Legacy(TEST)';
+        $car->Year=0000;
+        $car->save();
+        $this->assertTrue($car->delete());
     }
 }
