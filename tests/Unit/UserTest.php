3e00f7df4acada5db83be66f24fba339f63e81af
Rati <rjp76@njit.edu>
2018-07-24 11:57:27 -0400

    user insert unit test fixed so unique email constraint always works

--- tests/Unit/UserTest.php
+++ tests/Unit/UserTest.php
@@ -19,7 +19,7 @@ class UserTest extends TestCase
     {
         $user = new User();
         $user->name='Jane Doe';
-        $user->email ='blank@example.com';
+        $user->email = str_random(10).'@gmail.com';
         $user-> password = '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm'; // secret
         $user -> remember_token = str_random(10);
         $this->assertTrue($user->save());
